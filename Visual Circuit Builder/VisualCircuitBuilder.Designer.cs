3701ab6d16ba23a358a7fd7e3cef3eb327ff124c
Contingent101 <31139184+Contingent101@users.noreply.github.com>
2018-01-30 13:21:08 -0600

    Add files via upload

---
+++ Visual Circuit Builder/VisualCircuitBuilder.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Visual_Circuit_Builder
+{
+    partial class VCB_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.operatorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.truthConnectorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.menuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.operatorsToolStripMenuItem,
+            this.truthConnectorsToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menuStrip.Size = new System.Drawing.Size(591, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // operatorsToolStripMenuItem
+            // 
+            this.operatorsToolStripMenuItem.Name = "operatorsToolStripMenuItem";
+            this.operatorsToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.operatorsToolStripMenuItem.Text = "Operators";
+            // 
+            // truthConnectorsToolStripMenuItem
+            // 
+            this.truthConnectorsToolStripMenuItem.Name = "truthConnectorsToolStripMenuItem";
+            this.truthConnectorsToolStripMenuItem.Size = new System.Drawing.Size(114, 20);
+            this.truthConnectorsToolStripMenuItem.Text = "Truth Connectors ";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Size = new System.Drawing.Size(591, 379);
+            this.splitContainer1.SplitterDistance = 197;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // VCB_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(591, 403);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "VCB_Form";
+            this.Text = "Visual Circuit Builder";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem operatorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem truthConnectorsToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ Visual Circuit Builder/VisualCircuitBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visual_Circuit_Builder
+{
+    public partial class VCB_Form : Form
+    {
+        public VCB_Form()
+        {
+            InitializeComponent();
+            Console.WriteLine("Hello World");
+        }
+    }          
+}
